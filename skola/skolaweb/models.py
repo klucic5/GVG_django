98b3d2b494f2711004866fecf575356ab28ad5bb
klucic5 <48219043+klucic5@users.noreply.github.com>
2021-12-07 13:18:00 +0100

    Delete skola directory

--- skola/skolaweb/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Ucenik(models.Model):
-	Ime=models.CharField(max_length=20)
-	Prezime =models.CharField(max_length=20)
-	DatumRodenja=models.DateField()
-	def __repr__(self):
-		return self.Ime+' '+ self.Prezime
-
--- skola/skolaweb/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-urlpatterns=[
-	url(r'^ucenici/add',views.ucenici_add, name='uc_add'),
-	url(r'^ucenici/',views.ucenici_index, name='uc_index'),
-	url(r'^$',views.index, name='index'),
-	]
